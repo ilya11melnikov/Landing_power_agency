2dc2f12247a1b93488055c44b9764a16836ed5a8
Ilya <ilya11melnikov@gmail.com>
2024-11-24 20:53:22 +0200

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
 const body = document.querySelector('body');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+      loop: true, // Зациклювання слайдера
+      slidesPerView: 1, // Кількість видимих слайдів
+      spaceBetween: 30, // Відстань між слайдами
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 5000, // Автоперехід кожні 5 секунд
+      },
+    });
+  });
+  
