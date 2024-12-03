3313eb55a20643cdc35117f851964682f7bb1b03
Ilya <ilya11melnikov@gmail.com>
2024-12-03 19:53:58 +0200

    feat: vidgets, left hero

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,4 @@
 const swiperTwo=new Swiper(".swiper_2",{slidesPerView:5,loop:!0}),swiperOne=new Swiper(".swiper_1",{initialSlide:2,thumbs:{swiper:swiperTwo}});
-// const sliderName = new Swiper(".swiperClass", {
-//     navigation: {
-//         nextEl: '.btn_1',
-//         prevEl: '.btn_2'
-//     },
-//     pagination: {
-//         el: '.pagination_1',
-//         clickable: true,
-//     },
-//     mousewheel: true,
-//     Keyboard: true
-// });
 
 const swiperThree = new Swiper('.swiperClass3', {
     loop: true,
@@ -21,6 +9,6 @@ const swiperThree = new Swiper('.swiperClass3', {
         disableOnInteraction: false,
     },
     loopFillGroupWithBlank: true,
-    slidesPerGroup: 1, // Сколько слайдов прокручивать за раз
-    speed: 1000, // Скорость анимации в миллисекундах
+    slidesPerGroup: 1,
+    speed: 1000, 
 });
